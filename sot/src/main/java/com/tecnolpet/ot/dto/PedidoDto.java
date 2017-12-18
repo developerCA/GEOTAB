500ca7a44d65b1262aa2ffb6cbe340d30d97b78f
Christian Aguilar Poveda <developersystems@gmail.com>
2017-12-18 10:42:18 -0500

    Add files via upload

---
+++ sot/src/main/java/com/tecnolpet/ot/dto/PedidoDto.java
@@ -0,0 +1,71 @@
+package com.tecnolpet.ot.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.tecnolpet.ot.jview.ViewOT;
+import com.tecnolpet.ot.model.DetalleNotaPedido;
+import com.tecnolpet.ot.model.NotaPedido;
+import com.tecnolpet.ot.model.TareaDetalleNotaPedido;
+
+public class PedidoDto implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6734720136207777014L;
+	@JsonView(ViewOT.PublicView.class)
+	private NotaPedido notaPedido;
+	@JsonView(ViewOT.PublicView.class)
+	private List<DetalleNotaPedido> detalles;
+	@JsonView(ViewOT.PublicView.class)
+	private List<TareaDetalleNotaPedido> detalleServicios;
+	@JsonView(ViewOT.PublicView.class)
+	private DescuentoTotalDto descuento;
+	@JsonView(ViewOT.PublicView.class)
+	private double descuentoTotal;
+	@JsonView(ViewOT.PublicView.class)
+	private Integer totalServicios;
+	
+	public PedidoDto(){
+		detalleServicios=new ArrayList<TareaDetalleNotaPedido>();
+	}
+	public NotaPedido getNotaPedido() {
+		return notaPedido;
+	}
+	public void setNotaPedido(NotaPedido notaPedido) {
+		this.notaPedido = notaPedido;
+	}
+	public List<DetalleNotaPedido> getDetalles() {
+		return detalles;
+	}
+	public void setDetalles(List<DetalleNotaPedido> detalles) {
+		this.detalles = detalles;
+	}
+	public DescuentoTotalDto getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(DescuentoTotalDto descuento) {
+		this.descuento = descuento;
+	}
+	public double getDescuentoTotal() {
+		return descuentoTotal;
+	}
+	public void setDescuentoTotal(double descuentoTotal) {
+		this.descuentoTotal = descuentoTotal;
+	}
+	public Integer getTotalServicios() {
+		return totalServicios;
+	}
+	public void setTotalServicios(Integer totalServicios) {
+		this.totalServicios = totalServicios;
+	}
+	public List<TareaDetalleNotaPedido> getDetalleServicios() {
+		return detalleServicios;
+	}
+	public void setDetalleServicios(List<TareaDetalleNotaPedido> detalleServicios) {
+		this.detalleServicios = detalleServicios;
+	}
+	
+}
